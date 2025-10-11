0697ee457f0f2b3225c9e0f882c6a483741482ef
Gigi-Pons <grecia.atx@gmail.com>
2025-10-10 20:19:10 -0500

    implemented create controller method returning ResponseEntity

--- todo-app/src/main/java/com/greciabueno/todo_app/controller/TaskController.java
+++ todo-app/src/main/java/com/greciabueno/todo_app/controller/TaskController.java
@@ -3,19 +3,33 @@ package com.greciabueno.todo_app.controller;
 import com.greciabueno.todo_app.model.Task;
 import com.greciabueno.todo_app.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/tasks")
 public class TaskController {
     @Autowired
     private TaskService service;
 
-    @PostMapping
-    public Task create(@RequestBody Task task) {
-        return service.add(task);
+    @PostMapping("/add")
+    public ResponseEntity<Task> create(
+            @RequestBody Task task,
+            UriComponentsBuilder uriBuilder) {
+
+        Task saved = service.add(task);
+
+        //assign location header pointing to where the new resource lives
+        var uri = uriBuilder
+                .path("/tasks/add/{id}")
+                .buildAndExpand(saved.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(saved);
     }
+
+//    @PatchMapping("/{id}/completed")
+//    public Res
 }
